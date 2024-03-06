7eac907056079d3f635bdd5ade3c5fc028a9f2f5
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2024-03-06 22:52:47 +0200

    feat: added api folder

--- api/server.js
+++ api/server.js
@@ -5,11 +5,10 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-server.use(
-    jsonServer.rewriter({
-        'db.json': '/$1',
-    }),
-);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router);
 server.listen(3001, () => {
     console.log('JSON Server is running');
