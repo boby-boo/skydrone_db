62f0448a57ab3384d727de0b80b60a85ddd94fd6
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2024-03-06 23:25:22 +0200

    feat: added all contants in server.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 
-const fs = require("fs")
-const path = require("path")
-const db = JSON.parse(fs.readFileSync(path.join("db.json")))
 
-const router = jsonServer.router(db);
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+
+const router = jsonServer.router(db)
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
